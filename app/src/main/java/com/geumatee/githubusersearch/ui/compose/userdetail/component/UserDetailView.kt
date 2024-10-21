7cd07fb246b710a31f9c62f0dbaf5352786d58d2
geo jariyavorakij <geo.jariyavorakij@karabiner.tech>
2024-10-21 13:13:13 +0900

    refactor: extract followers and following string resource

--- app/src/main/java/com/geumatee/githubusersearch/ui/compose/userdetail/component/UserDetailView.kt
+++ app/src/main/java/com/geumatee/githubusersearch/ui/compose/userdetail/component/UserDetailView.kt
@@ -16,9 +16,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.geumatee.githubusersearch.R
 
 @Composable
 internal fun UserDetailView(
@@ -53,9 +55,9 @@ internal fun UserDetailView(
                         name ?: "",
                         modifier = Modifier.weight(1f)
                     )
-                    StatCounter("followers", followers)
+                    StatCounter(stringResource(R.string.followers), followers)
                     Spacer(modifier = Modifier.width(8.dp))
-                    StatCounter("following", following)
+                    StatCounter(stringResource(R.string.following), following)
                 }
 
             else -> Column(
