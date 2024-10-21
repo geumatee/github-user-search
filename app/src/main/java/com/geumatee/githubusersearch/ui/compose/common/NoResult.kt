a23ae96cfc3022e48af4150fef4847c5634576b5
geo jariyavorakij <geo.jariyavorakij@karabiner.tech>
2024-10-21 12:31:43 +0900

    feat: align center NoResult text

--- app/src/main/java/com/geumatee/githubusersearch/ui/compose/common/NoResult.kt
+++ app/src/main/java/com/geumatee/githubusersearch/ui/compose/common/NoResult.kt
@@ -9,6 +9,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.geumatee.githubusersearch.R
@@ -22,7 +23,10 @@ internal fun NoResult(modifier: Modifier = Modifier) {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = stringResource(R.string.no_results))
+        Text(
+            text = stringResource(R.string.no_results),
+            textAlign = TextAlign.Center
+        )
     }
 }
 
